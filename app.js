8dbdbd944713aefecf9d9b9c308ce2211316be0b
Jay <jdragon2050@gmail.com>
2023-10-20 23:39:07 -0700

    added background color change when user presses start, and if they get it wrong, start button is enabled again

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-const box1 = document.getElementById("box-1");
+const randomNumber = Math.floor(Math.random() * 5);
+const box = document.getElementById(`box-${randomNumber}`);
 const boxes = document.querySelectorAll(".box-container");
 const startBtn = document.querySelector(".start-btn");
-
-// boxes.forEach((box) => {
-//   box.addEventListener("click", (e) => {
-//     if (startBtn.disabled) {
-
-//     }
-//   });
-// });
+let boxesArray = [];
 
 startBtn.addEventListener("click", function () {
   this.disabled = true;
   this.style.cursor = "auto";
 });
+
+startBtn.addEventListener("click", function () {
+  startGame();
+
+  boxes.forEach((box) => {
+    box.addEventListener("click", function (e) {
+      if (e.target.classList.contains(`box-${randomNumber}`)) {
+        console.log("correct");
+      } else {
+        startBtn.disabled = false;
+      }
+    });
+  });
+});
+
+function startGame() {
+  if (startBtn.disabled) {
+    box.style.background = "blue";
+  }
+  setTimeout(() => {
+    box.style.background = "#f1f1f1";
+  }, 1000);
+  boxesArray.push(randomNumber);
+}

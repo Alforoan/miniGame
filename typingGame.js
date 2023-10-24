705de2f67f298ae34006d43f17aeda9dc54b2528
Jay <jdragon2050@gmail.com>
2023-10-24 16:50:54 -0700

    added fetchwords functionality as well as hilighting letters that are being typed

--- typingGame.js
+++ typingGame.js
@@ -1,22 +1,49 @@
 let wordCount = 500;
-const url = `https://random-word-api.vercel.app/api?words=${wordCount}`;
 let wordsArray = [];
+const url = `https://random-word-api.vercel.app/api?words=${wordCount}`;
+const wordsArraySpan = document.querySelector(".words-array");
 const startBtn = document.querySelector(".start-btn");
+const input = document.querySelector(".input");
 
-startBtn.addEventListener("click", function () {
-  fetch(url)
+startBtn.addEventListener("click", () => loadWords(20));
+
+async function loadWords(numberOfWords) {
+  inputArray = [];
+  currentIndex = 0;
+  wordsArraySpan.innerHTML = "";
+  const randomWords = await fetch(url)
     .then((data) => data.json())
     .then((data) => {
       const filteredData = data.filter((word) => {
-        return word.length < 7;
+        return word.length < 8;
       });
-      return filteredData;
-    })
-    .then((filteredData) => {
-      wordsArray = filteredData.slice(0, 10);
-      wordsArraySpan.textContent = wordsArray.join(" ");
-    })
-    .catch((error) => {
-      console.error("Error:", error);
+      return filteredData.slice(0, numberOfWords);
     });
+
+  //wordsArraySpan.textContent = randomWords.join(" ");
+  randomWords
+    .join(" ")
+    .split("")
+    .forEach((char) => {
+      const charSpan = document.createElement("span");
+      charSpan.innerText = char;
+      wordsArraySpan.appendChild(charSpan);
+    });
+
+  //input.value = "";
+}
+
+//below is the second best
+input.addEventListener("input", function () {
+  let randomWordsArray = wordsArraySpan.textContent;
+  let characters = wordsArraySpan.querySelectorAll("span");
+  let typedChar = input.value.split("");
+
+  for (let i = 0; i < characters.length; i++) {
+    if (i < typedChar.length && typedChar[i] === randomWordsArray[i]) {
+      characters[i].classList.add("highlight");
+    } else {
+      characters[i].classList.remove("highlight");
+    }
+  }
 });

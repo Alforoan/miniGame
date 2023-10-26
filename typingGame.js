d1b1a1438484e01f7bbbbf9acabe2b0c2f87cc02
Jay <jdragon2050@gmail.com>
2023-10-26 15:06:53 -0700

    added a fuctionality for hilighting the next word once previous word is completed

--- typingGame.js
+++ typingGame.js
@@ -5,6 +5,7 @@ const wordsArraySpan = document.querySelector(".words-array");
 const resetBtn = document.querySelector(".reset-btn");
 const input = document.querySelector(".input");
 const refreshBtn = document.querySelector(".fa-refresh");
+let randWordsArray = [];
 
 loadWords(20);
 resetBtn.addEventListener("click", () => loadWords(20));
@@ -26,7 +27,7 @@ async function loadWords(numberOfWords) {
   for (let i = 0; i < randomWords.length; i++) {
     randomWords[i] = randomWords[i] + " ";
   }
-  console.log(randomWords);
+  randWordsArray = [...randomWords];
 
   for (let i = 0; i < randomWords.length; i++) {
     const wordSpan = document.createElement("span");
@@ -48,76 +49,42 @@ async function loadWords(numberOfWords) {
 
   characters[0].classList.add("current-letter-highlight");
 }
+
+const game = {
+  mistakes: 0,
+  i: 0,
+  j: 0,
+};
+
+let gameArray = "";
 function startGame() {
   let randomWordsArray = wordsArraySpan.textContent;
   let characters = wordsArraySpan.querySelectorAll(".letter-span");
+  let words = wordsArraySpan.querySelectorAll(".word-span");
   let typedChar = input.value.split("");
-  let mistakes = 0;
+  let currentWord = words[game.i].querySelectorAll(".letter-span");
+
   characters[0].classList.remove("current-letter-highlight");
-  for (let i = 0; i < characters.length; i++) {
-    if (i === typedChar.length) {
-      characters[i].classList.add("current-letter-highlight");
-    } else {
-      characters[i].classList.remove("current-letter-highlight");
+
+  characters[0].classList.remove("current-letter-highlight");
+
+  if (typedChar.join("") === randWordsArray[game.i]) {
+    words[game.i].classList.add("word-highlight");
+    input.value = "";
+    for (let i = 0; i < randWordsArray[game.i].length; i++) {
+      gameArray += randWordsArray[game.i][i];
     }
-    if (i < typedChar.length && typedChar[i] === randomWordsArray[i]) {
-      characters[i].classList.add("highlight");
-    } else if (i < typedChar.length && typedChar[i] !== randomWordsArray[i]) {
-      characters[i].classList.add("mistake");
-      mistakes++;
-    } else {
-      characters[i].classList.remove("highlight");
-    }
+
+    game.i++;
+
+    game.j = 0;
+  }
+  console.log("game.j", game.j);
+  console.log(randomWordsArray[game.j]);
+  console.log(randomWordsArray[game.j + 1]);
+
+  if (typedChar[game.j] === randWordsArray[game.i][game.j]) {
+    currentWord[game.j].classList.add("highlight");
+    game.j++;
   }
 }
-
-//dont change
-// async function loadWords(numberOfWords) {
-//   inputArray = [];
-//   currentIndex = 0;
-//   wordsArraySpan.innerHTML = "";
-//   const randomWords = await fetch(url)
-//     .then((data) => data.json())
-//     .then((data) => {
-//       const filteredData = data.filter((word) => {
-//         return word.length < 8;
-//       });
-//       return filteredData.slice(0, numberOfWords);
-//     });
-
-//   randomWords
-//     .join(" ")
-//     .split("")
-//     .forEach((char) => {
-//       const charSpan = document.createElement("span");
-//       charSpan.innerText = char;
-//       wordsArraySpan.appendChild(charSpan);
-//     });
-
-//   input.value = "";
-//   let characters = wordsArraySpan.querySelectorAll("span");
-//   characters[0].classList.add("current-letter-highlight");
-// }
-
-// function startGame() {
-//   let randomWordsArray = wordsArraySpan.textContent;
-//   let characters = wordsArraySpan.querySelectorAll("span");
-//   let typedChar = input.value.split("");
-//   let mistakes = 0;
-//   characters[0].classList.remove("current-letter-highlight");
-//   for (let i = 0; i < characters.length; i++) {
-//     if (i === typedChar.length) {
-//       characters[i].classList.add("current-letter-highlight");
-//     } else {
-//       characters[i].classList.remove("current-letter-highlight");
-//     }
-//     if (i < typedChar.length && typedChar[i] === randomWordsArray[i]) {
-//       characters[i].classList.add("highlight");
-//     } else if (i < typedChar.length && typedChar[i] !== randomWordsArray[i]) {
-//       characters[i].classList.add("mistake");
-//       mistakes++;
-//     } else {
-//       characters[i].classList.remove("highlight");
-//     }
-//   }
-// }

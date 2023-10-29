fad46a5e9d1e31fbab280e362741660bfb41a841
Jay <jdragon2050@gmail.com>
2023-10-29 16:59:06 -0700

    added three letter words fetching and functions

--- wordScramble.js
+++ wordScramble.js
@@ -2,6 +2,31 @@ const firstRowContainer = document.querySelector(".first-row-container");
 const test = document.querySelector(".test");
 
 let fiveLetterWordsArray = [];
+let threeLetterWordsArray = [];
+
+async function fetchthreeLetterWordsData() {
+  fetch("./threeLetterWords.txt")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      let wordsList = data.split("\n");
+      wordsList = wordsList.map((word) => word.replace(/\r/g, ""));
+      let num1 = randomNumberGenerator(wordsList);
+      let num2 = randomNumberGenerator(wordsList);
+      let num3 = randomNumberGenerator(wordsList);
+      threeLetterWordsArray.push(
+        wordsList[num1],
+        wordsList[num2],
+        wordsList[num3]
+      );
+      console.log(threeLetterWordsArray);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+fetchthreeLetterWordsData();
 
 async function fetchFiveLetterWordsData() {
   fetch("./fiveLetterWords.txt")
@@ -25,12 +50,25 @@ async function fetchFiveLetterWordsData() {
       console.error("Error fetching data:", error);
     });
 }
-fetchFiveLetterWordsData();
 
-function addWords() {
+function addFiveLetterWords() {
   for (let i = 0; i < fiveLetterWordsArray.length; i++) {
     const wordDiv = document.createElement("div");
     const word = fiveLetterWordsArray[i];
+    for (let j = 0; j < word.length; j++) {
+      const char = word[j];
+      const charSpan = document.createElement("span");
+      charSpan.innerHTML = char;
+      wordDiv.appendChild(charSpan);
+    }
+    firstRowContainer.appendChild(wordDiv);
+  }
+}
+
+function addThreeLetterWords() {
+  for (let i = 0; i < threeLetterWordsArray.length; i++) {
+    const wordDiv = document.createElement("div");
+    const word = threeLetterWordsArray[i];
     for (let j = 0; j < word.length; j++) {
       const char = word[j];
       const charSpan = document.createElement("span");
@@ -48,7 +86,7 @@ function randomNumberGenerator(arr) {
 
 test.addEventListener("click", function () {
   console.log("test");
-  addWords();
+  addThreeLetterWords();
 });
 
-addWords();
+addThreeLetterWords();

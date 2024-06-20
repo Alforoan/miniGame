2cb90197afd0101bb2ab35c6d982cb34118cbee3
Jay <jdragon2050@gmail.com>
2024-06-20 15:03:21 -0700

    add error validation for signup and login pages

--- accounts/views.py
+++ accounts/views.py
@@ -10,19 +10,16 @@ def login_view(request):
   if request.user.is_authenticated:
     return redirect("recall_it")
   if request.method == "POST":
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-      login(request, user)
-      return redirect("recall_it")
-    else:
-      return render(request, "accounts/login.html", {
-        "message": "Invalid username and/or password."
-      })
-  return render(request, "accounts/login.html", {
-    "form": form
-  })
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      user = authenticate(request, username=username, password=password)
+      if user is not None:
+          login(request, user)
+          return redirect("recall_it")
+      else:
+          error_message = "Invalid username or password. Please try again."
+          return render(request, "accounts/login.html", {"form": form, "error_message": error_message})
+  return render(request, "accounts/login.html", {"form": form})
 
 def logout_view(request):
   logout(request)
@@ -38,7 +35,8 @@ def signup_view(request):
     password_confirmation = request.POST.get('password_confirmation')
     if username and password and password_confirmation:
       if password != password_confirmation:
-        return render(request, 'accounts/signup.html', {'form': form, 'error': 'The passwords do not match'})
+        error_message = "Passwords do not match. Please try again."
+        return render(request, 'accounts/signup.html', {'form': form, 'error_message': error_message})
       else:
         try:
           user = User.objects.create_user(username, password=password)

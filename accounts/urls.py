446120284b9b77653d74c46e6c79a8d23f6cedec
Jay <jdragon2050@gmail.com>
2024-06-24 14:43:18 -0700

    add account settings and change password setting

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import signup_view, login_view, logout_view
+from .views import signup_view, login_view, logout_view, account_view,change_password_view
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('account/', account_view, name='account'),
+    path('change_password/', change_password_view, name='change_password')
 ]
